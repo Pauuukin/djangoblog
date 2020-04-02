b3d02cf1c19398109fbfa1b0a33c737b8eb3ada3
pauuukin <oleg.paukin@mail.ru>
2020-04-03 00:27:14 +0200

    add routing and base template

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 
 # Create your views here.
+
+def posts_list(request):
+    names = ['Oleg', 'Masha', 'Vanya', 'Nastya']
+    return render(request, 'blog/index.html', context={'names': names})
