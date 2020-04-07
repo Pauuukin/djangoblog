eef4221965a5a21bef3567b6a8eb8e7ef202f687
pauuukin <oleg.paukin@mail.ru>
2020-04-07 15:04:33 +0200

    added class views and Mixin Class for Detail obj

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpRequest
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import View
 from .models import *
-
-# Create your views here.
+from .utils import ObjectDetailMixin
 
 
 def posts_list(request):
@@ -10,9 +9,18 @@ def posts_list(request):
     return render(request, 'blog/index.html', context={'posts': posts})
 
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
+# def post_detail(request, slug):
+#     post = Post.objects.get(slug__iexact=slug)
+#     return render(request, 'blog/post_detail.html', context={'post': post})
+
+
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+    # def get(self, request, slug):
+    #     # post = Post.objects.get(slug__iexact=slug)
+    #     post = get_object_or_404(Post, slug__iexac=slug)
+    #     return render(request, 'blog/post_detail.html', context={'post': post})
 
 
 def tags_list(request):
@@ -20,6 +28,7 @@ def tags_list(request):
     return render(request, 'blog/tags_list.html', context={'tags': tags})
 
 
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag': tag})
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
