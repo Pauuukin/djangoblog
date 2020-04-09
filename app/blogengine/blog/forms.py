fae24335f9181cd8db6ea16b45a3ed754dc12fb0
pauuukin <oleg.paukin@mail.ru>
2020-04-09 20:59:42 +0200

    created a form to add posts

--- app/blogengine/blog/forms.py
+++ app/blogengine/blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Tag
+from .models import *
 from django.core.exceptions import ValidationError
 
 
@@ -38,3 +38,21 @@ class TagForm(forms.ModelForm):
     #     return new_tag
 
 
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'slug', 'body', 'tags']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'body': forms.SelectMultiple(attrs={'class': 'form-control'})
+        }
+
+    def clean_slug(self):
+        """очищаем данные: запрещаем создавать объекты с slug == create и условие UNIQUE"""
+        new_slug = self.cleaned_data['slug'].lower()
+        if new_slug == 'create':
+            raise ValidationError('Slug may not be "Create"')
+        return new_slug
--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from .models import *
+
 from .utils import ObjectDetailMixin
 from .forms import *
 
@@ -45,3 +45,16 @@ class TagCreate(View):
             new_tag = bound_form.save()
             return redirect(new_tag)
         return render(request, 'blog/tag_create.html', context={'form': bound_form})
+
+
+class PostCreate(View):
+    def get(self, request):
+        form = PostForm()
+        return render(request, 'blog/post_create_form.html', context={'form': form})
+
+    def post(self, request):
+        bound_form = PostForm(request.POST)
+        if bound_form.is_valid():
+            new_post = bound_form.save()
+            return redirect(new_post)
+        return render(request, 'blog/post_create_form.html', context={'form': bound_form})
