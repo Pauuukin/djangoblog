801ea6679515ad7a1b075398e917ae34fcf57eae
pauuukin <oleg.paukin@mail.ru>
2020-04-10 17:13:22 +0200

    added the ability to edit posts and tags

--- app/blogengine/blog/models.py
+++ app/blogengine/blog/models.py
@@ -28,6 +28,9 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def get_update_url(self):
+        return reverse('post_update_url', kwargs={'slug': self.slug})
+
 
 class Tag(models.Model):
     title = models.CharField(max_length=50)
@@ -39,6 +42,8 @@ class Tag(models.Model):
     def __str__(self):
         return self.title
 
+    def get_update_url(self):
+        return reverse('tag_update_url', kwargs={'slug': self.slug})
 
 
 
--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -4,6 +4,8 @@ from django.views.generic import View
 from .utils import *
 from .forms import *
 
+#   --------------------------posts-----------------------------
+
 
 def posts_list(request):
     posts = Post.objects.all()
@@ -24,6 +26,31 @@ class PostDetail(ObjectDetailMixin, View):
     #     return render(request, 'blog/post_detail.html', context={'post': post})
 
 
+class PostCreate(ObjectCreateMixin, View):
+    model_form = PostForm
+    template = 'blog/post_create_form.html'
+
+    # def get(self, request):
+    #     form = PostForm()
+    #     return render(request, 'blog/post_create_form.html', context={'form': form})
+    #
+    # def post(self, request):
+    #     bound_form = PostForm(request.POST)
+    #     if bound_form.is_valid():
+    #         new_post = bound_form.save()
+    #         return redirect(new_post)
+    #     return render(request, 'blog/post_create_form.html', context={'form': bound_form})
+
+
+class PostUpdate(ObjectUpdateMixin, View):
+    model = Post
+    model_form = PostForm
+    template = 'blog/post_update_form.html'
+
+
+#   --------------------------tags-----------------------------
+
+
 def tags_list(request):
     tags=Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags': tags})
@@ -39,17 +66,21 @@ class TagCreate(ObjectCreateMixin, View):
     template = 'blog/tag_create.html'
 
 
-class PostCreate(ObjectCreateMixin, View):
-    model_form = PostForm
-    template = 'blog/post_create_form.html'
+class TagUpdate(ObjectUpdateMixin, View):
+    model = Tag
+    model_form = TagForm
+    template = 'blog/tag_update_form.html'
 
-    # def get(self, request):
-    #     form = PostForm()
-    #     return render(request, 'blog/post_create_form.html', context={'form': form})
+    # def get(self, request, slug):
+    #     tag = Tag.objects.get(slug__iexact=slug)
+    #     bound_form = TagForm(instance=tag)
+    #     return  render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag': tag})
     #
-    # def post(self, request):
-    #     bound_form = PostForm(request.POST)
+    # def post(self, request, slug):
+    #     tag = Tag.objects.get(slug__iexact=slug)
+    #     bound_form = TagForm(request.POST, instance=tag)
+    #
     #     if bound_form.is_valid():
-    #         new_post = bound_form.save()
-    #         return redirect(new_post)
-    #     return render(request, 'blog/post_create_form.html', context={'form': bound_form})
+    #         new_tag = bound_form.save()
+    #         return redirect(new_tag)
+    #     return render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag': tag})
