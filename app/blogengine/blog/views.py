e1fe72858d2a03584a64b82e4f3cd342d141b3f1
pauuukin <oleg.paukin@mail.ru>
2020-04-10 17:47:05 +0200

    added the ability to delete posts and tags

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-
+from django.urls import reverse
 from .utils import *
 from .forms import *
 
@@ -48,6 +48,12 @@ class PostUpdate(ObjectUpdateMixin, View):
     template = 'blog/post_update_form.html'
 
 
+class PostDelete(ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete_form.html'
+    redirect_url = 'posts_list_url'
+
+
 #   --------------------------tags-----------------------------
 
 
@@ -84,3 +90,18 @@ class TagUpdate(ObjectUpdateMixin, View):
     #         new_tag = bound_form.save()
     #         return redirect(new_tag)
     #     return render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag': tag})
+
+
+class TagDelete(ObjectDeleteMixin, View):
+    model = Tag
+    template = 'blog/tag_delete_form.html'
+    redirect_url = 'tags_list_url'
+
+    # def get(self, request, slug):
+    #     tag = Tag.objects.get(slug__iexact = slug)
+    #     return render(request, 'blog/tag_delete_form.html', context={'tag': tag})
+    #
+    # def post(self,request, slug):
+    #     tag = Tag.objects.get(slug__iexact=slug)
+    #     tag.delete()
+    #     return redirect(reverse('tags_list_url'))
